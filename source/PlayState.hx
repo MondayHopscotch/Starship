5aa0ba1677257a898c81f3f7277f0ebad7433560
Logan <monday.hopscotch@gmail.com>
2020-04-23 00:13:21 -0600

    controller support. tow distance controls

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,6 +5,8 @@ import flixel.FlxState;
 import flixel.addons.nape.FlxNapeSpace;
 import flixel.addons.nape.FlxNapeSprite;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import input.BasicControls;
 import nape.constraint.DistanceJoint;
 import nape.constraint.LineJoint;
 import nape.constraint.PulleyJoint;
@@ -18,38 +20,43 @@ import nape.shape.Polygon;
 import openfl.display.FPS;
 
 class PlayState extends FlxState {
-	var fps:FPS;
+	static inline var MIN_TOW_DISTANCE:Float = 10;
+
+	var controls:BasicControls;
 
 	private static inline var TERRAIN_GROUP:Int = 1;
 	private static inline var SHIP_GROUP:Int = 2;
 	private static inline var CARGO_GROUP:Int = 4;
 
-	var gravity:Vec2 = Vec2.get().setxy(0, 200);
-
 	var ship:FlxNapeSprite;
 	var cargo:FlxNapeSprite;
 
 	// Units: Pixels/sec/sec
-	var enginePower:Vec2 = Vec2.get().setxy(10, 0);
+	var gravity:Vec2 = Vec2.get().setxy(0, 200);
+	var enginePower:Vec2 = Vec2.get().setxy(500, 0);
+
+	// Units: Pixels/sec
+	var grappleRate:Float = 50;
 
 	// Units: Rads/sec
-	var TURN_POWER:Float = 40;
+	var TURN_POWER:Float = 2;
 
 	var jointed:Bool = false;
 	var joint:DistanceJoint = null;
 
 	override public function create() {
 		super.create();
+
 		FlxNapeSpace.init();
 		FlxNapeSpace.drawDebug = true;
-
 		FlxNapeSpace.createWalls(0, 0, 0, 0);
-
-		createTestObj();
 		FlxNapeSpace.space.gravity.set(gravity);
+
+		createTestObjs();
+		setupControls();
 	}
 
-	function createTestObj() {
+	function createTestObjs() {
 		ship = new FlxNapeSprite();
 		ship.setPosition(300, 300);
 		ship.loadGraphic(AssetPaths.shot__png);
@@ -63,10 +70,10 @@ class PlayState extends FlxState {
 		add(ship);
 
 		cargo = new FlxNapeSprite();
-		cargo.setPosition(300, 330);
+		cargo.setPosition(50, FlxG.height - 50);
 		var cargoBody = new Body(BodyType.DYNAMIC);
 		cargoBody.shapes.add(new Polygon(Polygon.rect(-5, -5, 10, 10)));
-		cargoBody.gravMassScale = 5;
+		cargoBody.mass *= 5;
 
 		var cargoFilter = new InteractionFilter(CARGO_GROUP, ~(SHIP_GROUP));
 		cargoBody.setShapeFilters(cargoFilter);
@@ -74,11 +81,41 @@ class PlayState extends FlxState {
 		cargo.addPremadeBody(cargoBody);
 		add(cargo);
 
-		joint = new DistanceJoint(ship.body, cargo.body, Vec2.weak(0, 0), Vec2.weak().setxy(0, 0), 0, 100);
+		joint = new DistanceJoint(ship.body, cargo.body, Vec2.weak(0, 0), Vec2.weak().setxy(0, 0), MIN_TOW_DISTANCE, 100);
 		joint.stiff = false;
 		joint.frequency = 20;
-		joint.damping = 1;
+		joint.damping = 4;
 		joint.space = FlxNapeSpace.space;
+		joint.breakUnderError = true;
+		joint.active = false;
+
+		createBucket(50, FlxG.height - 50, 50, 50);
+		createBucket(FlxG.width - 50, FlxG.height - 50, 50, 50);
+	}
+
+	function createBucket(x:Int, y:Int, width:Int, height:Int) {
+		var wallThickness = 10;
+		var left = new FlxNapeSprite();
+		left.setPosition(x - width / 2 - wallThickness, y + height / 2);
+		left.createRectangularBody(wallThickness, height);
+		left.body.type = BodyType.STATIC;
+		add(left);
+
+		var right = new FlxNapeSprite();
+		right.setPosition(x + width / 2, y + height / 2);
+		right.createRectangularBody(wallThickness, height);
+		right.body.type = BodyType.STATIC;
+		add(right);
+
+		var bottom = new FlxNapeSprite();
+		bottom.setPosition(x, y + height);
+		bottom.createRectangularBody(width, wallThickness);
+		bottom.body.type = BodyType.STATIC;
+		add(bottom);
+	}
+
+	function setupControls() {
+		controls = new BasicControls();
 	}
 
 	override public function update(elapsed:Float) {
@@ -86,15 +123,27 @@ class PlayState extends FlxState {
 
 		ship.body.angularVel *= .95;
 
-		if (FlxG.keys.pressed.A) {
-			ship.body.applyAngularImpulse(-TURN_POWER);
+		if (controls.thruster.x > 0.1) {
+			FlxG.watch.addQuick("Thruster     : ", controls.thruster.x);
+			ship.body.applyImpulse(Vec2.weak().set(enginePower).mul(elapsed).mul(controls.thruster.x).rotate(ship.body.rotation));
 		}
 
-		if (FlxG.keys.pressed.D) {
-			ship.body.applyAngularImpulse(TURN_POWER);
+		if (Math.abs(controls.steer.x) > 0.1) {
+			FlxG.watch.addQuick("Steering     : ", controls.steer.x);
+			// ship.body.applyAngularImpulse(TURN_POWER * controls.steer.x);
+			ship.body.angularVel = TURN_POWER * controls.steer.x;
 		}
 
-		if (FlxG.mouse.justPressed) {
+		if (Math.abs(controls.grappleAdjust.y) > 0.1) {
+			FlxG.watch.addQuick("GrappleAdjust: ", controls.grappleAdjust.y);
+			if (jointed) {
+				joint.jointMax += grappleRate * elapsed * controls.grappleAdjust.y;
+				joint.jointMax = Math.max(MIN_TOW_DISTANCE, joint.jointMax);
+				FlxG.watch.addQuick("Tow Length    : ", joint.jointMax);
+			}
+		}
+
+		if (controls.toggleGrapple.check()) {
 			if (jointed) {
 				joint.active = false;
 				jointed = false;
@@ -103,11 +152,6 @@ class PlayState extends FlxState {
 				joint.active = true;
 				jointed = true;
 			}
-		}
-
-		if (FlxG.keys.pressed.SPACE) {
-			ship.body.applyImpulse(Vec2.weak().set(enginePower).rotate(ship.body.rotation));
-			// ship.body.applyImpulse(Vec2.weak().set(enginePower).rotate(-Math.PI / 2));
 		}
 	}
 }
---
+++ source/input/BasicControls.hx
@@ -0,0 +1,39 @@
+package input;
+
+import flixel.FlxG;
+import flixel.input.actions.FlxAction.FlxActionAnalog;
+import flixel.input.actions.FlxAction.FlxActionDigital;
+import flixel.input.actions.FlxActionManager;
+
+class BasicControls {
+	static var actions:FlxActionManager;
+
+	public var thruster:FlxActionAnalog;
+	public var steer:FlxActionAnalog;
+	public var grappleAdjust:FlxActionAnalog;
+	public var toggleGrapple:FlxActionDigital;
+
+	public function new() {
+		thruster = new FlxActionAnalog("thruster");
+		steer = new FlxActionAnalog("steer");
+		grappleAdjust = new FlxActionAnalog("grappleAdjust");
+		toggleGrapple = new FlxActionDigital("toggleGrapple");
+
+		if (actions == null) {
+			actions = FlxG.inputs.add(new FlxActionManager());
+		}
+		actions.addActions([thruster, steer, grappleAdjust, toggleGrapple]);
+
+		// thruster.addKey(FlxKey.SPACE, JUST_PRESSED);
+		toggleGrapple.addKey(E, JUST_PRESSED);
+
+		updateGamepadInput();
+	}
+
+	function updateGamepadInput() {
+		thruster.addGamepad(RIGHT_TRIGGER, MOVED);
+		steer.addGamepad(LEFT_ANALOG_STICK, MOVED);
+		grappleAdjust.addGamepad(RIGHT_ANALOG_STICK, MOVED);
+		toggleGrapple.addGamepad(B, JUST_PRESSED);
+	}
+}
